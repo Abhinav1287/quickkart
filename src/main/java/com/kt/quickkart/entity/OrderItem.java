c98f286609db72e69a5f91e3999970fe9555e245
Srishti Tiwari <srishti.tiwari0503@gmail.com>
2025-07-24 00:42:41 +0530

    Entity

---
+++ src/main/java/com/kt/quickkart/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.kt.quickkart.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Order;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/kt/quickkart/entity/Product.java
@@ -0,0 +1,32 @@
+package com.kt.quickkart.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    private String name;
+    private String description;
+    private double price;
+    private String createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @OneToMany(mappedBy = "product")
+    private List<OrderItem> orderItems;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+}
