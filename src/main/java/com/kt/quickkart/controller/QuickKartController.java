1d985897e47e55ecdb8e299fe1f48aca35a2218a
Akshita Singh <akshitasingh20@gmail.com>
2025-07-24 17:15:01 +0530

    add category and add product api

--- src/main/java/com/kt/quickkart/controller/QuickKartController.java
+++ src/main/java/com/kt/quickkart/controller/QuickKartController.java
@@ -7,6 +7,8 @@ import com.kt.quickkart.repository.CategoryRepo;
 import com.kt.quickkart.repository.UserRepo;
 import com.kt.quickkart.service.QuickKartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,5 +42,14 @@ public class QuickKartController {
         user.setUserId(null);
         return userRepo.save(user);
     }
-
+    @PostMapping("/addcategory")
+    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+        Category savedCategory = quickKartService.addCategory(category);
+        return new ResponseEntity<>(savedCategory, HttpStatus.CREATED);
+    }
+    @PostMapping("/addproduct")
+    public ResponseEntity<Product> addProduct(@RequestBody Product product, @RequestParam Long categoryId) {
+        Product savedProduct = quickKartService.addProduct(product, categoryId);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/kt/quickkart/entity/Product.java
+++ src/main/java/com/kt/quickkart/entity/Product.java
@@ -1,5 +1,6 @@
 package com.kt.quickkart.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class Product {
 
     @ManyToOne
     @JoinColumn(name = "category_id")
+    @JsonIgnore
     private Category category;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
